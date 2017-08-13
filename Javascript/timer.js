b0f2b45905c44ecb36fc6b022ea44d4af512e072
Ariel Tay <shihqian@gmail.com>
2017-08-13 16:16:53 +0800

    Edited modal border + Size. Added rows in Modal-Body

--- Javascript/timer.js
+++ Javascript/timer.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate =  Date.now();
+var countDownDate = new Date();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
